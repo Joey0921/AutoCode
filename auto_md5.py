39a40724710b2c146739edc4fcda6a132f623d35
joey <935626276@qq.com>
2023-03-02 11:43:04 +0800

    md5自动生成代码

--- auto_md5.py
+++ auto_md5.py
@@ -19,10 +19,6 @@ tablename = fn[0][0:-4]
 txtfn = tablename + '_md5'
 sqlfile = open(txtfn,'a')
 
-#生成insert语句
-sqlfile.writelines("insert overwrite table udm_idz." + tablename + "_md5 \nselect\n")
-
-
 #拼接字段
 ziduan = ''
 for row in range(1,rows):
@@ -35,9 +31,6 @@ for row in range(1,rows):
         ziduan =  ziduan + columns[6:-1] + ',        --' + comment[6:-1] + '\n'
     else:
         continue
-
-#拼接字段
-sqlfile.writelines(ziduan + "MD5(CONCAT_WS(','\n")
 
 #拼接MD5计算字段
 md5zd = ''
@@ -54,8 +47,6 @@ for row in range(1,rows):
     else:
         continue
 
-sqlfile.writelines(md5zd + ')) AS md5str\n')
-
 #取新增字段
 addzd = ''
 for row in range(1,rows):
@@ -69,8 +60,6 @@ for row in range(1,rows):
     else:
         continue
 
-sqlfile.writelines(addzd)
-
 #取分区字段
 partition = ''
 for row in range(1,rows):
@@ -82,8 +71,6 @@ for row in range(1,rows):
         partition =  partition + "," + values[6:-1] + " as " + columns[6:-1] + "\n"
     else:
         continue
-
-sqlfile.writelines(partition)
 
 #取系统分区字段
 sourceid = ''
@@ -98,4 +85,11 @@ for row in range(1,rows):
     else:
         continue
 
-sqlfile.writelines("from udm_idz." + tablename + "_md5 where " + sourceid)
+#拼接字段
+sqlfile.writelines("insert overwrite table udm_idz." + tablename + "_md5 \nselect\n"
+                   + ziduan + "MD5(CONCAT_WS(','\n"
+                   + md5zd + ')) AS md5str\n'
+                   + addzd
+                   + partition
+                   + "from udm_idz." + tablename + "_md5 where " + sourceid
+                   )
